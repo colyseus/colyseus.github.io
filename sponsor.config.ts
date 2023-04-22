3d23d0260f28dd168b4ecedc01fc8ca9438a5c0f
Endel Dreyer <endel.dreyer@gmail.com>
2023-04-22 18:26:01 -0300

    add sponsorkit

---
+++ sponsor.config.ts
@@ -0,0 +1,50 @@
+import { defineConfig, presets } from 'sponsorkit';
+
+export default defineConfig({
+  // includePrivate: true,
+  tiers: [
+    {
+      title: 'Past Sponsors',
+      monthlyDollars: -1,
+      preset: presets.xs,
+    },
+
+    {
+      title: 'Backers',
+      // to replace the entire tier rendering
+      // compose: (composer, tierSponsors, config) => {
+      //   composer.addRaw(
+      //     '<-- custom svg -->',
+      //   )
+      // },
+    },
+
+    {
+      title: 'Sponsors',
+      monthlyDollars: 100,
+      preset: presets.base,
+      // to insert custom elements after the tier block
+      composeAfter: (composer, _tierSponsors, _config) => {
+        composer.addSpan(10)
+      },
+    },
+
+    {
+      title: 'Silver Sponsors',
+      monthlyDollars: 250,
+      preset: presets.medium,
+    },
+
+    {
+      title: 'Gold Sponsors',
+      monthlyDollars: 500,
+      preset: presets.large,
+    },
+
+    {
+      title: 'Platinum Sponsors',
+      monthlyDollars: 1000,
+      preset: presets.xl,
+    },
+  ],
+});
