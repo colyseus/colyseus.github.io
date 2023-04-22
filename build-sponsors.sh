7b071a9a1a38a75f01763a2dc97ff5a43b19f166
Endel Dreyer <endel.dreyer@gmail.com>
2023-04-22 18:39:45 -0300

    fix sponsorkit workflow

---
+++ build-sponsors.sh
@@ -0,0 +1,4 @@
+npx sponsorkit -w=800
+npx sponsorkit -w=1800 --name=sponsors.wide
+npx sponsorkit -w=800 --name=sponsors.part1 --filter=">50"
+npx sponsorkit -w=800 --name=sponsors.part2 --filter="<=50"
