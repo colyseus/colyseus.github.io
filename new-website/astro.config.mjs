fbbbbdbecd984610f11e6b997ab135c2fc8604f9
Endel Dreyer <endel.dreyer@gmail.com>
2023-09-03 15:02:26 -0300

    upgrade astro. add 'deploying to colyseus cloud' section

--- new-website/astro.config.mjs
+++ new-website/astro.config.mjs
@@ -1,16 +1,20 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import react from '@astrojs/react';
+import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://colyseus.io',
   compressHTML: true,
-  integrations: [mdx(), sitemap(), tailwind(), react()],
-  // adapter: netlify(),
+  integrations: [expressiveCode(), mdx(), sitemap(), tailwind(), react()],
 
-  experimental: { assets: true },
+  //
+  // Syntax Highlight docs:
+  // ----------------------
+  // https://docs.astro.build/en/guides/markdown-content/#syntax-highlighting
+  //
+
 });
