10f70fed1069249995f8fb3e46aeba46d49c7413
Endel Dreyer <endel.dreyer@gmail.com>
2023-09-18 16:04:36 -0300

    improve open graph tags

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Colyseus';
-export const SITE_DESCRIPTION = 'Colyseus Multiplayer Framework for Node.js';
+export const SITE_DESCRIPTION = 'Colyseus Multiplayer Framework for Node.js / Bun';
