813c1c41f7d7d2a28e236ad7f61db98c5d827dfe
Endel Dreyer <endel.dreyer@gmail.com>
2023-09-27 10:55:25 -0300

    add @sominator godot video

--- src/indexes/community.ts
+++ src/indexes/community.ts
@@ -2,47 +2,15 @@
 import { getOpenGraph } from "../utils/getOpenGraphImage";
 
 export const communityLibrary = [
+  await getOpenGraph("https://www.youtube.com/watch?v=9QtQ5iXj2U0", {}),
   await getOpenGraph("https://github.com/sunflower-land/sunflower-land", {}),
-
-  await getOpenGraph("https://github.com/keldaanCommunity/pokemonAutoChess", {
-    // title: "Protocol Buffers Message Exchange",
-    // description: "Demonstrating Raw Binary Message Exchange"
-  }),
-
-  await getOpenGraph("https://github.com/orion3dgames/t5c", {
-    // title: "Basic Examples"
-  }),
-
-  await getOpenGraph("https://github.com/halftheopposite/TOSIOS", {
-    // title: "Basic Examples"
-  }),
-
-  await getOpenGraph("https://github.com/damian-pastorini/reldens", {
-    // title: "Reldens"
-    // description: "A platform to create Multiplayer 2d RPG games."
-  }),
-
-  await getOpenGraph("https://github.com/stopnoanime/21-online", {
-    // title: "Push to Talk",
-    // description: `Simple "Push To Talk" proof of concept / example.`
-  }),
-
-  await getOpenGraph("https://www.youtube.com/watch?v=5XaGzRH5Y7I", {
-    // title: "Basic Examples"
-  }),
-
-  await getOpenGraph("https://www.youtube.com/watch?v=NylOh_PVCsc", {
-    // title: "Tic-Tac-Toe",
-    // description: "Multiplayer Tic-Tac-Toe demonstrating state synchronization. Client-side using PixiJS or Defold."
-  }),
-
-  await getOpenGraph("https://www.youtube.com/playlist?list=PLNwtXgWIx3rhOmsJOkUFRiFQg8pPFPDqy", {
-    // title: "Firebase Auth Demo",
-    // description: "Authentication demonstration using Firebase"
-  }),
-
-  await getOpenGraph("https://www.youtube.com/playlist?list=PLxgtJR7f0RBK_yGDSbPuspqMR-oEi1S25", {
-    // title: "Decentraland Scene"
-  }),
-
+  await getOpenGraph("https://github.com/keldaanCommunity/pokemonAutoChess", { }),
+  await getOpenGraph("https://github.com/orion3dgames/t5c", { }),
+  await getOpenGraph("https://github.com/halftheopposite/TOSIOS", { }),
+  await getOpenGraph("https://github.com/damian-pastorini/reldens", { }),
+  await getOpenGraph("https://github.com/stopnoanime/21-online", { }),
+  await getOpenGraph("https://www.youtube.com/watch?v=5XaGzRH5Y7I", { }),
+  await getOpenGraph("https://www.youtube.com/watch?v=NylOh_PVCsc", { }),
+  await getOpenGraph("https://www.youtube.com/playlist?list=PLNwtXgWIx3rhOmsJOkUFRiFQg8pPFPDqy", { }),
+  await getOpenGraph("https://www.youtube.com/playlist?list=PLxgtJR7f0RBK_yGDSbPuspqMR-oEi1S25", { }),
 ];
