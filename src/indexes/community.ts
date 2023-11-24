c765bc2b4467ba9855b8db4c87999e02dac407f0
Endel Dreyer <endel.dreyer@gmail.com>
2023-11-24 15:13:18 -0300

    community project: deniztraka/dtworldz.multiplayer-turn-based-strategy

--- src/indexes/community.ts
+++ src/indexes/community.ts
@@ -2,6 +2,7 @@
 import { getOpenGraph } from "../utils/getOpenGraphImage";
 
 export const communityLibrary = [
+  await getOpenGraph("https://github.com/deniztraka/dtworldz.multiplayer-turn-based-strategy", {}),
   await getOpenGraph("https://portal.gitnation.org/contents/building-team-thinking-games-at-synthesis", {}),
   await getOpenGraph("https://www.youtube.com/watch?v=9QtQ5iXj2U0", {}),
   await getOpenGraph("https://github.com/sunflower-land/sunflower-land", {}),
