5d6fe371db99197222fae75ba9af4493e010d1ea
Endel Dreyer <endel.dreyer@gmail.com>
2024-01-16 14:42:58 -0300

    update sponsorkit img

--- build-sponsors.sh
+++
@@ -1,4 +0,0 @@
-npx sponsorkit -w=800 --outputDir dist/sponsorkit
-npx sponsorkit -w=1800 --name=sponsors.wide --outputDir dist/sponsorkit
-npx sponsorkit -w=800 --name=sponsors.part1 --filter=">50" --outputDir dist/sponsorkit
-npx sponsorkit -w=800 --name=sponsors.part2 --filter="<=50" --outputDir dist/sponsorkit
