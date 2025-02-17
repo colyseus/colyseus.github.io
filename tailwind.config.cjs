81d34b1e70ecd932bd68f18f88bafdbbed04258f
Endel Dreyer <endel.dreyer@gmail.com>
2025-02-17 12:12:34 -0300

    update docs links

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,24 @@ module.exports = {
       fontFamily: {
         sans: ['Inter', ...defaultTheme.fontFamily.sans],
         cinzel: ['Cinzel', 'serif'],
+      },
+
+      keyframes: {
+        leftToRight: {
+          '0%': { transform: 'translateX(1em) translateY(0.5em)', opacity: 0 },
+          '100%': { transform: 'translateX(0)', opacity: 1  },
+        },
+        rightToLeft: {
+          '0%': { transform: 'translateX(0)', opacity: 0 },
+          '100%': { transform: 'translateX(1em) translateY(0.5em)', opacity: 1  },
+        }
+      },
+
+      animation: {
+        leftToRight: 'leftToRight 1s cubic-bezier(0.33, 1, 0.68, 1) forwards',
+        rightToLeft: 'rightToLeft 1s cubic-bezier(0.33, 1, 0.68, 1) forwards',
       }
+
     },
 	},
 	plugins: [],
