25978d11f05150647e22744133bf79e92bf5af84
Endel Dreyer <endel.dreyer@gmail.com>
2026-03-07 14:49:35 -0300

    wording

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Colyseus';
-export const SITE_DESCRIPTION = 'Colyseus Multiplayer Framework for Node.js / Bun';
+export const SITE_DESCRIPTION = 'Ship multiplayer games faster with Colyseus. The developer-friendly real-time multiplayer framework for JavaScript/TypeScript.';
