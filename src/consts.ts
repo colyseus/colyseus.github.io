1b02f3f61ac51116a507876d61c680c62e9f6ee5
Endel Dreyer <endel.dreyer@gmail.com>
2026-03-08 21:24:47 -0300

    seo

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Colyseus';
-export const SITE_DESCRIPTION = 'Ship multiplayer games faster with Colyseus. The developer-friendly real-time multiplayer framework for JavaScript/TypeScript.';
+export const SITE_DESCRIPTION = 'Build and ship real-time multiplayer experiences with Colyseus. Open-source framework for Node.js with built-in matchmaking, state sync, and SDKs for Unity, Defold, Haxe, and JavaScript.';
